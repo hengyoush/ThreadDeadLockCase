f70ea19ef2369f9b8d6eb35ae9f3a275b1ec7df7
春丸 <hengyoush1@163.com>
2024-02-23 01:47:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 mvn clean package
 docker build -t hengyoush/my-spring-boot-app:$EXP_ID .
+docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
+docker push docker.io/hengyoush/my-spring-boot-app:$EXP_ID
