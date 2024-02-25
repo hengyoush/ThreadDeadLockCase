81b8329cc369ef6f6e45dc6a08df9f2c163943f3
有点小邪恶 <hengyoush1@163.com>
2024-02-26 00:29:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git config --global --add safe.directory /home/coder/project
 git config --global user.email "you@example.com"
+git add -all
 git commit -m "commit"
 export git_url=$(git remote get-url --push origin) \
 && export github_token=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 \
