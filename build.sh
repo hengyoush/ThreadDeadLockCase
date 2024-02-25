b0f0e5768bb0e684d57d2f8d4ade07223c88422a
有点小邪恶 <hengyoush1@163.com>
2024-02-26 01:17:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ git config --global user.email "you@example.com"
 git config --global user.name "you"
 git add --all
 git commit -m "commit"
-export git_username=$GITHUB_USER && export git_url=$(git remote get-url --push origin) \
-&& export github_token=$GITHUB_TOKEN  \
-&&  echo "git push $git_url" | sed -e "s|https://|https://$git_username:$github_token@|" | sh
+GITHUB_USER=$1
+GITHUB_TOKEN=$2
+export git_url=$(git remote get-url --push origin) \
+&&  echo "git push $git_url" | sed -e "s|https://|https://$GITHUB_USER:$GITHUB_TOKEN@|" | sh
 
