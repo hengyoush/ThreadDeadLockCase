77c871758cc9c4112a96caaecac918d3a5f22b8b
有点小邪恶 <hengyoush1@163.com>
2024-02-26 00:54:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 git config --global --add safe.directory /home/coder/project
 git config --global user.email "you@example.com"
+git config --global user.name "you"
 git add --all
 git commit -m "commit"
-export git_url=$(git remote get-url --push origin) \
-&& export github_token=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 \
-&&  echo "git push $git_url" | sed -e "s|https://|https://$github_token@|"  | sh
+export git_username=hengyoush && export git_url=$(git remote get-url --push origin) \
+&& export github_token=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  \
+&&  echo "git push $git_url" | sed -e "s|https://|https://$git_username:$github_token@|" | sh
