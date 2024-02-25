8502ecc78531e569545f73c7fa75863ac4d510c0
有点小邪恶 <hengyoush1@163.com>
2024-02-26 00:28:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-mvn -s /opt/settings.xml clean package
-docker build -t hengyoush/my-spring-boot-app:$EXP_ID .
-docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
-docker push docker.io/hengyoush/my-spring-boot-app:$EXP_ID
+git config --global --add safe.directory /home/coder/project
+git config --global user.email "you@example.com"
+git commit -m "commit"
+export git_url=$(git remote get-url --push origin) \
+&& export github_token=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 \
+&&  echo "git push $git_url" | sed -e "s|https://|https://$github_token@|"  | sh
