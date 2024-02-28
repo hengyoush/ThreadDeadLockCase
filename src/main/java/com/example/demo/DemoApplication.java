33910f9b7f8cee4a5e1ea9fae8babec0a627ec2b
hengyoush <hengyoush1@163.com>
2024-02-28 16:21:18 +0000

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // 123
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
