91d75ba9bc8f4db54caa9acc0c16488f3d9acef4
root <root@localhost.localdomain>
2024-03-01 00:20:33 +0800

    不让Java进程退出导致容器退出

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,5 +5,7 @@ DEFAULT_JVM_PARAMS="-Xmx512m"
 # 外部传入的JVM参数，可以根据实际需求修改
 JVM_PARAMS="${JVM_PARAMS:-$DEFAULT_JVM_PARAMS}"
 
-# 启动Java应用，使用exec形式确保Java进程不成为init进程
-exec java $JVM_PARAMS -jar app.jar
+# 启动Java应用，使用exec形式确保Java进程不成为init进程，并在后台运行
+exec java $JVM_PARAMS -jar your-spring-app.jar &
+# 添加无限循环，以防止脚本退出
+tail -f /dev/null
