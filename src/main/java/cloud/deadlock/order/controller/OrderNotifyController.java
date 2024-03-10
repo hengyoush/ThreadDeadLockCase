a9afd9db03d73c169f9080a073ce625ce4ea5b5b
hengyoush <hengyoush1@163.com>
2024-03-10 13:52:01 +0000

    commit

--- src/main/java/cloud/deadlock/order/controller/OrderNotifyController.java
+++ src/main/java/cloud/deadlock/order/controller/OrderNotifyController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
-public class OrderNotifyController {
+public class OrderNotifyController { 
 
     @Autowired
     private OrderNotifyService orderNotifyService;
