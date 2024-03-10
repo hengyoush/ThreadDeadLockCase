dce9bca5e4e3fa1e140a2de84e2355cfdb4377c7
hengyoush <hengyoush1@163.com>
2024-03-10 13:50:24 +0000

    commit

--- src/main/java/cloud/deadlock/order/controller/OrderNotifyController.java
+++ src/main/java/cloud/deadlock/order/controller/OrderNotifyController.java
@@ -5,7 +5,7 @@ import cloud.deadlock.order.entity.OrderNotifyRequest;
 import cloud.deadlock.order.service.OrderNotifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
